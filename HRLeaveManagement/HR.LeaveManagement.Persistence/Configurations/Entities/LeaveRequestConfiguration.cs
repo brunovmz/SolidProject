173563f757bbfdf8e72c13d5b08cc66da7702ef3
bruno valverde <br1.vmz@gmail.com>
2022-05-05 21:42:59 -0300

    unit test added and DB seeded

--- HRLeaveManagement/HR.LeaveManagement.Persistence/Configurations/Entities/LeaveRequestConfiguration.cs
+++ HRLeaveManagement/HR.LeaveManagement.Persistence/Configurations/Entities/LeaveRequestConfiguration.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace HR.LeaveManagement.Persistence.Configurations.Entities
 {
-    public class LeaveRequestConfiguration
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
     {
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+            
+        }
     }
 }
